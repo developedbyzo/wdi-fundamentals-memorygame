2de1a21fcbc04577bda1fa0ce74de713e8914b33
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-11-18 14:48:51 -0500

    modified JS

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,8 +1,23 @@
 //console.log("Up and running!")
+//alert('Hello, Friends! ')
 
-var cardOne = "queen"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "king"
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
-console.log("User flipped" + " " +cardThree)
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+/* var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardsInPlay = [cardOne, cardTwo];
+cardsInPlay.push(cardOne || cardTwo);
+
+if(cardsInPlay.length === 2) {
+    alert("You found a match!")
+}else if(cardsInPlay[0] === cardsInPlay[2]) {
+    alert("Sorry, try again.")
+}
+console.log("User flipped " + cardsInPlay);*/
