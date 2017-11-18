388e2276b5143e8343b379133a41a3c4333c835f
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-11-18 17:59:15 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,13 +1,26 @@
 //console.log("Up and running!")
 //alert('Hello, Friends! ')
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [cardOne, cardTwo];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2) {
+    alert("You found a match!");
+}else if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("Sorry, try again")
+}
+
+
 
 /* var cards = ["queen", "queen", "king", "king"];
 var cardOne = cards[0];
