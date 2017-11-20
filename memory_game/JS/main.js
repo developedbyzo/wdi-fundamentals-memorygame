c7e35ed59f6282369cfdbcc097ecc6d878b1c439
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-11-20 09:06:36 -0500

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -7,30 +7,18 @@ var cardThree = "king";
 var cardFour = "king";*/
 
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cardOne, cardTwo];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2) {
-    alert("You found a match!");
-}else if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("Sorry, try again")
-}
-
-
-
-/* var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardsInPlay = [cardOne, cardTwo];
-cardsInPlay.push(cardOne || cardTwo);
-
-if(cardsInPlay.length === 2) {
-    alert("You found a match!")
-}else if(cardsInPlay[0] === cardsInPlay[2]) {
-    alert("Sorry, try again.")
-}
-console.log("User flipped " + cardsInPlay);*/
+var cardsInPlay = [cards];
+var checkForMatch = function () {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again")
+    }
+};
+var flipCard = function (cardId) {
+    checkForMatch();
+    console.log("User flipped " + cards[cardId]);
+};
+cardsInPlay.push(cards[cardId]);
+var cardId = flipCard(0);
+             flipCard(2);
