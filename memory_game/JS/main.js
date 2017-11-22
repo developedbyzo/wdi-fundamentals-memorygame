7cc609b666bbd82e312532b7f6f792e7d6037a77
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-11-22 17:15:39 -0500

    memory game

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -9,18 +9,18 @@ var cardFour = "king"; */
 var cards = [
   {
     rank: 'queen',
-    suit: 'hearts',
-    cardImage: 'images/queen-of-hearts.png'
-  },
-  {
-    rank: 'queen',
     suit: 'diamonds',
     cardImage: 'images/queen-of-diamonds.png'
   },
   {
-    rank: 'king',
+    rank: 'queen',
     suit: 'hearts',
-    cardImage: 'images/king-of-hearts.png'
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
   },
   {
     rank: 'king',
@@ -28,21 +28,38 @@ var cards = [
     cardImage: 'images/king-of-hearts.png'
   }
 ]
-var cardsInPlay = [cards]
+var cardsInPlay = []
 var checkForMatch = function () {
+//   this.cardImage.setAttribute(src)
+  if (cardsInPlay.length === 2) {
+  }
   if (cardsInPlay[0] === cardsInPlay[1]) {
     window.alert('You found a match!')
   } else {
     window.alert('Sorry, try again')
   }
-};
-
-var flipCard = function (cardId) {
+}
+var flipCard = function () {
+  console.log('flipCard() ran')
+  var cardId = this.getAttribute('data-id')
+//   cardId.getAttribute('data-id')
+  this.setAttribute('src', cards[cardId].cardImage)
   checkForMatch()
+  cardsInPlay.push(cards[cardId])
   console.log('User flipped ' + cards[cardId].rank)
   console.log('User flipped ' + cards[cardId].cardImage)
   console.log('User flipped ' + cards[cardId].suit)
 }
-var cardId = flipCard(0)
-flipCard(2)
-cardsInPlay.push(cards[cardId]);
+function createBoard () {
+  var gameBoard = document.getElementById('game-board')
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img')
+    cardElement.setAttribute('src', 'images/back.png')
+    cardElement.setAttribute('data-id', i)
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement)
+  }
+}
+/*var cardId = flipCard(0)
+flipCard(2)*/
+createBoard()
